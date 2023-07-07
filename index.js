99d28069252d2fcbee36d5e3d4b52f4e6a19a15c
Ashish Tiwari <rkt987186@gmail.com>
2023-07-08 02:35:02 +0530

    cookie issue

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const jwt = require('jsonwebtoken');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const { createProduct } = require('./controller/Product');
+const cookieParser =require('cookie-parser')
 const productsRouter = require('./routes/Products');
 const categoriesRouter = require('./routes/Categories');
 const brandsRouter = require('./routes/Brands');
@@ -18,15 +19,18 @@ const authRouter = require('./routes/Auth');
 const cartRouter = require('./routes/Cart');
 const ordersRouter = require('./routes/Order');
 const { User } = require('./model/User');
-const { isAuth, sanitizeUser } = require('./services/common');
+const { isAuth, sanitizeUser, cookieExtractor } = require('./services/common');
 
 const SECRET_KEY = 'SECRET_KEY';
 // JWT options
+
 const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.jwtFromRequest = cookieExtractor;
 opts.secretOrKey = SECRET_KEY; // TODO: should not be in code;
 //middlewares
 
+server.use(express.static('build'))
+server.use(cookieParser());
 server.use(
     session({
       secret: 'keyboard cat',
@@ -34,6 +38,7 @@ server.use(
       saveUninitialized: false, // don't create session until something stored
     })
   );
+  
   server.use(passport.authenticate('session'));
   server.use(
     cors({
@@ -54,11 +59,13 @@ server.use('/orders', isAuth(), ordersRouter.router);
 // Passport Strategies
 passport.use(
   'local',
-  new LocalStrategy(async function (username, password, done) {
+  new LocalStrategy(
+    {usernameField:'email'},
+    async function (email, password, done) {
     // by default passport uses username
     try {
-      const user = await User.findOne({ email: username });
-      console.log(username, password, user);
+      const user = await User.findOne({ email: email });
+      console.log(email, password, user);
       if (!user) {
         return done(null, false, { message: 'invalid credentials' }); // for safety
       }
@@ -73,7 +80,7 @@ passport.use(
             return done(null, false, { message: 'invalid credentials' });
           }
           const token = jwt.sign(sanitizeUser(user), SECRET_KEY);
-          done(null, token); // this lines sends to serializer
+          done(null, {token}); // this lines sends to serializer
         }
       );
     } catch (err) {
@@ -87,7 +94,7 @@ passport.use(
   new JwtStrategy(opts, async function (jwt_payload, done) {
     console.log({ jwt_payload });
     try {
-      const user = await User.findOne({ id: jwt_payload.sub });
+      const user = await User.findById(jwt_payload.id);
       if (user) {
         return done(null, sanitizeUser(user)); // this calls serializer
       } else {
--- services/common.js
+++ services/common.js
@@ -7,3 +7,11 @@ exports.isAuth = (req, res, done) => {
 exports.sanitizeUser = (user)=>{
     return {id:user.id, role:user.role}
 }
+exports.cookieExtractor=function(req){
+    var token=null;
+    if(req&&req.cookies){
+        token=req.cookies['jwt'];
+    }
+    token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0YTg2NzMzYTE2Yzc4YTc3ZTJhMWUyOSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjg4NzYwNDgzfQ.Jpha86l8PXNGFsC9TyG2q64iQAGx-byaNTnNgZowlRg"
+    return token;
+}
